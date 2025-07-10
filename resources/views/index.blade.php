44f3e800da040595afaf810f33d70ccd5c42a634
fjkk <fujikake.takayoshi@gmail.com>
2025-07-10 20:20:10 +0900

    github actions test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,6 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            test
             @if (session('status'))
                 <div class="alert alert-success" role="alert">
                     {{ session('status') }}
